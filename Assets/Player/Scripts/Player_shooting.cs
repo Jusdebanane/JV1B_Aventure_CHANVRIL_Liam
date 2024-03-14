7b97ece48863d9f4613c31a70a5bffd756377083
Jusdebanane <118273696+Jusdebanane@users.noreply.github.com>
2024-03-14 18:59:04 +0100

    shooting

--- Assets/Player/Scripts/Player_shooting.cs
+++ Assets/Player/Scripts/Player_shooting.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player_shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform weapon;
+    public float offset;
+
+    public Transform shot_point;
+    public GameObject bullet;
+    public float shot_cooldown;
+    private float next_shot;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Vector3 displacement = weapon.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = -Mathf.Atan2(displacement.x, displacement.y) * Mathf.Rad2Deg;
+        weapon.rotation = Quaternion.Euler(0f,0f,angle + offset);
+
+        if(Input.GetMouseButton(0))
+        {
+            if(Time.time > next_shot)
+            {
+                next_shot = Time.time + shot_cooldown;
+                Instantiate(bullet,shot_point.position,shot_point.rotation);
+            }
+        }
     }
 }
